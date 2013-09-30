9ba1a42e3b6a2b9b4e100b5495e5ebf0665591d6
JLignell <johan_91_104@hotmail.com>
2013-09-30 22:01:09 +0200

    Visual Studio projekt skapat

---
+++ Documents/System/BUPSystem/Logic Layer/LogicController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic_Layer
+{
+    /// <summary>
+    /// Singelton Controller that is used for delegation from the GUI layer to the logic layer.
+    /// The GUI layers entrance point.
+    /// </summary>
+    /// <remarks></remarks>
+    public class LogicController
+    {
+        /// <summary>
+        /// Lazy Instance of LogicController Singelton
+        /// </summary>
+        private static readonly Lazy<LogicController> instance = new Lazy<LogicController>(() => new LogicController());
+
+        /// <summary>
+        /// Empty constructor.
+        /// </summary>
+        /// <remarks></remarks>
+        private LogicController()
+        {
+        }
+
+        /// <summary>
+        /// The instance property
+        /// </summary>
+        /// <remarks></remarks>
+        public static LogicController Instance
+        {
+            get { return instance.Value; }
+        }
+    }
+}
