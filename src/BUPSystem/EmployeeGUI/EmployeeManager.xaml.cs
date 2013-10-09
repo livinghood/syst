f9e32e4d6fab8df854ebc830453f6a136bd6c675
Jonny <jonnyc.klasson@gmail.com>
2013-10-09 12:26:56 +0200

    Fixat basen till employee management

--- src/BUPSystem/EmployeeGUI/EmployeeManager.xaml.cs
+++ src/BUPSystem/EmployeeGUI/EmployeeManager.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Windows;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using Logic_Layer;
 
 namespace BUPSystem.EmployeeGUI
 {
@@ -7,9 +11,48 @@ namespace BUPSystem.EmployeeGUI
     /// </summary>
     public partial class EmployeeManager : Window
     {
+
+        private Employee employee;
+
+        private bool changeExistingEmployee;
+
         public EmployeeManager()
         {
             InitializeComponent();
         }
+
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+            if (!changeExistingEmployee)
+            {
+                long employeeId = long.Parse(tbEmployeeID.Text);
+                int sallery = int.Parse(tbEmployeeSallary.Text);
+                int employeeRate = int.Parse(tbEmployeeRate.Text);
+                decimal vacancy = decimal.Parse(tbEmployeeVacancy.Text);
+                EmployeeManagement.Instance.CreateEmployee(employeeId, tbEmployeeName.Text, sallery, employeeRate, vacancy );
+            }
+            else
+            {
+                EmployeeManagement.Instance.UpdateEmployee();
+            }          
+        }
+
+        //Kontroll för att avdelningarna är = 0 om de inte är ifyllda
+        //Kontroll för vilka och hur många avdelningar som är ifyllda
+
+        //public void CheckDigitDotOnly(object sender, KeyPressEventArgs e)
+        //{ FUNKAR INTE EFTERSOM DET INTE ÄR WINFORMS
+        //    char ch = e.KeyChar;
+        //    if (!Char.IsDigit(ch) && ch != 8 && ch != 46)
+        //    {
+        //        e.Handled = true;
+        //    }
+        //}
+
+        private void tbEmployeeID_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+
+        }
+
     }
 }
--- src/Logic Layer/EmployeeManagement.cs
+++ src/Logic Layer/EmployeeManagement.cs
@@ -1,12 +1,122 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Logic_Layer.EmployeeNamespace
+namespace Logic_Layer
 {
     public class EmployeeManagement
     {
+        /// <summary>
+        /// Lazy Instance of EmployeeManager singelton
+        /// </summary>
+        private static readonly Lazy<EmployeeManagement> instance = new Lazy<EmployeeManagement>(() => new EmployeeManagement());
+
+        /// <summary>
+        /// Database context
+        /// </summary>
+        private readonly DatabaseConnection db = new DatabaseConnection();
+
+        /// <summary>
+        /// The instance property
+        /// </summary>
+        public static EmployeeManagement Instance
+        {
+            get { return instance.Value; }
+        }
+
+
+        /// <summary>
+        /// Get a list of all employees
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerable<Employee> GetEmployee()
+        {
+            IEnumerable<Employee> employees = from c in db.Employee
+                                              orderby c.EmployeeName
+                                              select c;
+
+            return employees;
+        }
+
+        /// <summary>
+        /// Create a new employee
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="name"></param>
+        /// <param name="sallary"></param>
+        /// <param name="employrate"></param>
+        /// <param name="vacancy"></param>
+        public void CreateEmployee(long id, string name, int sallary, int employrate, decimal vacancy)
+        {
+            Employee newEmployee = new Employee { EmployeeID = id, EmployeeName = name, MonthSallary = sallary, EmployeementRate = employrate, VacancyDeduction = vacancy };
+            db.Employee.Add(newEmployee);
+            db.SaveChanges();
+        }
+
+        /// <summary>
+        /// Delete a employee
+        /// </summary>
+        /// <param name="employee"></param>
+        public void DeleteEmployee(Employee employee)
+        {
+            db.Employee.Remove(employee);
+            db.SaveChanges();
+        }
+
+        /// <summary>
+        /// Update a employee
+        /// </summary>
+        public void UpdateEmployee()
+        {
+            db.SaveChanges();
+        }
+
+
+        //-----------------------------------------------------------------------------------------------------------
+
+
+        /// <summary>
+        /// Get a list of all employeeplacements
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerable<EmployeePlacement> GetEmployeePlacement()
+        {
+            IEnumerable<EmployeePlacement> placements = from c in db.EmployeePlacement
+                                                        orderby c.EmployeeID
+                                                        select c;
+
+            return placements;
+        }
+
+        /// <summary>
+        /// Create a new EmployeePlacement
+        /// </summary>
+        /// <param name="eId"></param>
+        /// <param name="dId"></param>
+        /// <param name="allocate"></param>
+        public void CreateEmployeePlacement(long eId, string dId, decimal allocate)
+        {
+            EmployeePlacement newEmployeePlacement = new EmployeePlacement { EmployeeID = eId, DepartmentID = dId, EmployeeAllocate = allocate };
+            db.EmployeePlacement.Add(newEmployeePlacement);
+            db.SaveChanges();
+        }
+
+        /// <summary>
+        /// Delete a EmployeePlacement
+        /// </summary>
+        /// <param name="employeePlacement"></param>
+        public void DeleteEmployeePlacement(EmployeePlacement employeePlacement)
+        {
+            db.EmployeePlacement.Remove(employeePlacement);
+            db.SaveChanges();
+        }
+
+        /// <summary>
+        /// Update a EmployeePlacement
+        /// </summary>
+        public void UpdateEmployeePlacement()
+        {
+            db.SaveChanges();
+        }
     }
 }
