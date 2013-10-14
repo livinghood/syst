e4391a3d64aa96ef713422ee08b6f2c54e8d4038
JLignell <johan_91_104@hotmail.com>
2013-10-14 10:40:30 +0200

    Sök och sorteringsklasser

---
+++ src/Logic Layer/General Logic/Search.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Logic_Layer.General_Logic
+{
+    public class Search
+    {
+        /// <summary>
+        /// Lazy Instance of UserManagement Singelton
+        /// </summary>
+        private static readonly Lazy<Search> instance = new Lazy<Search>(() => new Search());
+
+        /// <summary>
+        /// The instance property
+        /// </summary>
+        /// <remarks></remarks>
+        public static Search Instance
+        {
+            get { return instance.Value; }
+        }
+
+        public int UserRegisterSearch(string searchCriteria, IReadOnlyList<UserAccount> userAccountList)
+        {
+            int index = -1;
+
+            for (int i = 0; i < userAccountList.Count; i++)
+            {
+                if (userAccountList[i].UserName.ToLower().Contains(searchCriteria))
+                {
+                    index = i;
+                    break;
+                }
+            }
+            return index;
+        }
+    }
+}
---
+++ src/Logic Layer/General Logic/Sort.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic_Layer.General_Logic
+{
+    public static class Sort
+    {
+        /// <summary>
+        /// Sorts an IQueryable and returns it as an IEnumerable
+        /// </summary>
+        /// <typeparam name="T">Independent type</typeparam>
+        /// <param name="source">The query to sort</param>
+        /// <param name="propertyName">The property to sort by, e.g. ProductID or ProductName</param>
+        /// <param name="ascending">Determines sort direction</param>
+        /// <returns></returns>
+        public static IEnumerable<T> SortBy<T>(this IQueryable<T> source, string propertyName, bool ascending)
+        {
+            // If ascending is equal to true then sort direction will be ascending otherwise it'll be descending
+            string sortDirection = @ascending ? "OrderBy" : "OrderByDescending";
+
+            var parameter = Expression.Parameter(source.ElementType, String.Empty);
+            var property = Expression.Property(parameter, propertyName);
+            var lambda = Expression.Lambda(property, parameter);
+
+            var methodCallExpression = Expression.Call(typeof(Queryable), sortDirection,
+                new[] { source.ElementType, property.Type },
+                source.Expression, Expression.Quote(lambda));
+
+            var query = source.Provider.CreateQuery<T>(methodCallExpression);
+
+            return query.AsEnumerable();
+        }
+    }
+}
