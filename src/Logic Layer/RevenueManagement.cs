c364a966abd37d7f4e5955616c8bfd9d30a98c61
Jonny <jonnyc.klasson@gmail.com>
2013-10-15 09:48:54 +0200

    skapat manager för intaktsbudget

---
+++ src/Logic Layer/RevenueManagement.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic_Layer
+{
+    class RevenueManagement
+    {
+        /// <summary>
+        /// Lazy Instance of CustomerManager singelton
+        /// </summary>
+        private static readonly Lazy<RevenueManagement> instance = new Lazy<RevenueManagement>(() => new RevenueManagement());
+
+        /// <summary>
+        /// Database context
+        /// </summary>
+        private DatabaseConnection db = new DatabaseConnection();
+
+        /// <summary>
+        /// The instance property
+        /// </summary>
+        public static RevenueManagement Instance
+        {
+            get { return instance.Value; }
+        }
+
+        /// <summary>
+        /// Create a new customer
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="name"></param>
+        /// <param name="category"></param>
+        public void CreateFinancialIncome(FinancialIncome financialIncome)
+        {
+            db.FinancialIncome.Add(financialIncome);
+            db.SaveChanges();
+        }
+
+
+    }
+}
