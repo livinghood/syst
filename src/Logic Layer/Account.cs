0fa945201134ad43305c2d1c71d00375eee7b18c
Patrik Lundqvist <pat.lundqvist@gmail.com>
2013-10-29 14:36:15 +0100

    Fixed notify

--- src/Logic Layer/Account.cs
+++ src/Logic Layer/Account.cs
@@ -6,23 +6,56 @@
 //    Manual changes to this file will be overwritten if the code is regenerated.
 // </auto-generated>
 //------------------------------------------------------------------------------
+using System.ComponentModel;
 
 namespace Logic_Layer
 {
     using System;
     using System.Collections.Generic;
-    
-    public partial class Account
+
+    public partial class Account : INotifyPropertyChanged
     {
+        private int m_AccountID;
+        private string m_AccountName;
+        private int? m_AccountCost;
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+        protected bool SetField<T>(ref T field, T value, string propertyName)
+        {
+            if (EqualityComparer<T>.Default.Equals(field, value)) return false;
+            field = value;
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+
         public Account()
         {
             this.DirectActivityCost = new HashSet<DirectActivityCost>();
             this.DirectProductCost = new HashSet<DirectProductCost>();
         }
-    
-        public int AccountID { get; set; }
-        public string AccountName { get; set; }
-        public Nullable<int> AccountCost { get; set; }
+
+        public int AccountID
+        {
+            get { return m_AccountID; }
+            set { SetField(ref m_AccountID, value, "AccountID"); }
+        }
+        public string AccountName
+        {
+            get { return m_AccountName; }
+            set { SetField(ref m_AccountName, value, "AccountName"); }
+        }
+        public Nullable<int> AccountCost
+        {
+            get { return m_AccountCost; }
+            set { SetField(ref m_AccountCost, value, "AccountCost"); }
+        }
     
         public virtual ICollection<DirectActivityCost> DirectActivityCost { get; set; }
         public virtual ICollection<DirectProductCost> DirectProductCost { get; set; }
